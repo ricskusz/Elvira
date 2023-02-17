d342d9a894a864fc351c3417da3bf58dcb011ae6
megnemveszithetek <richarszab56@gmail.com>
2023-02-17 11:09:18 +0100

    Adatok kérése api-ból
    Buggos statek miatt nem jó még

--- src/components/AllomasokInput.jsx
+++ src/components/AllomasokInput.jsx
@@ -1,31 +1,78 @@
-import { useContext } from "react";
+import { useContext, useState} from "react";
 import TelepulesekContext from "../context/TelepulesekContext";
 import MenetrendContext from "../context/MenetrendContext";
 
-
 const AllomasokInput = () => {
 
-    const {setKiindulopont, setCelallomas} = useContext(MenetrendContext);
-    const {telepulesek} = useContext(TelepulesekContext);
+    const formTemplate = {
+        indulas: "",
+        celallomas: ""
+    }
+
+    const [formData, setFormData] = useState(formTemplate);
+
+    const {setVegpontok, getMenetrend} = useContext(MenetrendContext);
+    const { telepulesek } = useContext(TelepulesekContext);
 
     const getSubString = (string, char1, char2) => {
         return string.substring(
             string.indexOf(char1) + 1,
             string.lastIndexOf(char2)
-          );
+        );
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+        setVegpontok(formData);
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        getMenetrend();
+        setFormData(formTemplate);
     }
 
+
   return (
-    <select className="select select-bordered w-full max-w-xs">
-      <option disabled selected>
-        Válasszon 
-      </option>
-      {
-        telepulesek.split(',').map((telepules) => (
-            <option value={getSubString(telepules, '"', '"')}>{getSubString(telepules, '"', '"')}</option>
-        ))
-      }
-    </select>
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="d-flex flex-col">
+            <label htmlFor="indulas">Kiinduló pont</label>
+            <select 
+                className="select select-bordered w-full max-w-xs"
+                id="indulas"
+                onChange={handleChange}
+                value={formData.indulas}>
+                <option disabled selected>Válasszon kiinduló pontot</option>
+                {telepulesek.split(",").map((telepules) => (
+                    <option value={getSubString(telepules, '"', '"')}>
+                    {getSubString(telepules, '"', '"')}
+                    </option>
+                ))}
+            </select>
+        </div>
+        <div className="d-flex flex-col">
+            <label htmlFor="celallomas">Célállomás</label>
+            <select 
+                className="select select-bordered w-full max-w-xs"
+                id="celallomas"
+                onChange={handleChange}
+                value={formData.celallomas}
+            >
+                <option disabled selected>Válasszon célállomás</option>
+                {telepulesek.split(",").map((telepules) => (
+                    <option value={getSubString(telepules, '"', '"')}>
+                    {getSubString(telepules, '"', '"')}
+                    </option>
+                ))}
+            </select>
+        </div>
+        <button type="submit" className="btn">Keresés</button>
+      </form>
+    </div>
   );
 };
 
--- src/context/MenetrendContext.jsx
+++ src/context/MenetrendContext.jsx
@@ -1,25 +1,26 @@
-import {useState, createContext} from 'react'
+import {useState, createContext, useEffect} from 'react'
 
 const MenetrendContext = createContext();
 
 export const MenetrendProvider = ({children}) => {
 
     const [menetrend, setMenetrend] = useState({});
-    const [kiindulopont, setKindulopont] = useState("");
-    const [celalomas, setCelallomas] = useState("");
+    const [vegpontok, setVegpontok] = useState({});
 
     const getMenetrend = async () => {
-
-        const response = await fetch(`https://apiv2.oroszi.net/elvira?from=${kiindulopont}&to=${celalomas}`);
+        console.log(vegpontok.indulas)
+        const response = await fetch(`https://apiv2.oroszi.net/elvira?from=${vegpontok.indulas}&to=${vegpontok.celallomas}`);
         const valasz = await response.json();
         setMenetrend(valasz);
+        console.log(menetrend);
     }
 
-    return <MenetrendContext.Provider value={
+    return <MenetrendContext.Provider value = {
         {
             menetrend,
-            setKindulopont,
-            setCelallomas
+            setVegpontok,
+            getMenetrend,
+            vegpontok
         }
     }>{children}</MenetrendContext.Provider>
 }
