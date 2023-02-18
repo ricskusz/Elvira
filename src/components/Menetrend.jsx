b8c59093bd0e3463fc8e4a37bb4cbf0dc5f09b2e
Richard <richarszab56@gmail.com>
2023-02-18 15:49:03 +0100

    Elem csere
    Ha nincs adat nem jelenik meg semmi sem

--- src/components/Menetrend.jsx
+++ src/components/Menetrend.jsx
@@ -4,7 +4,7 @@ const Menetrend = ({adatok}) => {
   return (
     <div>
         {
-            adatok.length > 0 ? adatok.map((adat, index) => (<MenetrendAdat key={index} rekord={adat}/>)) : <>nincs adat</>
+            adatok.length > 0 ? adatok.map((adat, index) => (<MenetrendAdat key={index} rekord={adat}/>)) : <></>
         }
     </div>
   )
