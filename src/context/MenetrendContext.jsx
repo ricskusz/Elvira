f7ae619b234ed392e243ccc0ba8593e3b59fff30
Richard <richarszab56@gmail.com>
2023-02-18 13:39:07 +0100

    Pontosabb adatok
    Adatok megjelenítése végett új statek

--- src/context/MenetrendContext.jsx
+++ src/context/MenetrendContext.jsx
@@ -1,21 +1,29 @@
-import {useState, createContext, useEffect} from 'react'
+import {useState, createContext} from 'react'
 
 const MenetrendContext = createContext();
 
 export const MenetrendProvider = ({children}) => {
 
     const [menetrend, setMenetrend] = useState({});
+    const [datum, setDatum] = useState("");
+    const [route, setRoute] = useState("");
 
     const getMenetrend = async (vegpontok) => {
+
         const response = await fetch(`https://apiv2.oroszi.net/elvira?from=${vegpontok.indulas}&to=${vegpontok.celallomas}`);
-        const {timetable} = await response.json();
+        const {timetable, date, route} = await response.json();
+
         setMenetrend(timetable);
+        setDatum(date);
+        setRoute(route);
     }
 
     return <MenetrendContext.Provider value = {
         {
             menetrend,
             getMenetrend,
+            datum,
+            route
         }
     }>{children}</MenetrendContext.Provider>
 }
