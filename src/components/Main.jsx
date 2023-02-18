905c3ebd0fa795ca036c8d1642eba09b4faa5a08
Richard <richarszab56@gmail.com>
2023-02-18 13:02:44 +0100

    Csere
    Elemek cseréje mobilon is átlátható felépítés érdekében

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,12 @@ import AllomasokInput from "./AllomasokInput"
 const Main = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero-content flex-col lg:flex-row">
+                <AllomasokInput />
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Keressen most!</h1>
                     <p className="py-6">Menetrend listázó alkalmazás. Válassza ki honnan hova szeretne eljutni és listázzuk önnek a mai járatokat!</p>
                 </div>
-                <AllomasokInput />
             </div>
         </div>
     )
