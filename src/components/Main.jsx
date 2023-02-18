c056daefcd539a901f0fca01d2ff9b4a5638e18e
Richard <richarszab56@gmail.com>
2023-02-18 15:29:38 +0100

    Feltételes renderelés
    In page nvavigáció gomb töltéseffekt amíg tart a fetch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,36 @@
 import AllomasokInput from "./AllomasokInput"
+import FooldalSzoveg from "./FooldalSzoveg"
+import MenetrendAdat from "./MenetrendAdat"
+import Menetrend from "./Menetrend"
+import { useContext } from "react"
+import MenetrendContext from "../context/MenetrendContext"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const Main = () => {
+
+    const { menetrend, route, date, loading} = useContext(MenetrendContext);
+
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <AllomasokInput />
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Keressen most!</h1>
-                    <p className="py-6">Menetrend listázó alkalmazás. Válassza ki honnan hova szeretne eljutni és listázzuk önnek a mai járatokat!</p>
+        <div className="flex flex-col justify-center items-center">
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <AllomasokInput />
+                    <div className="text-center lg:text-left">
+                        <FooldalSzoveg />
+                        {
+                            loading ? <button className="btn btn-success loading"></button>
+                            :
+                            <div>
+                                <AnchorLink href="#talalatok"><button className="btn btn-active btn-success">Találatok ({menetrend.length})</button></AnchorLink>
+                            </div>
+                        }
+                    </div>
                 </div>
+            </div>
+            <div id="talalatok" className="my-5 text-center">
+                <h1 className="text-3xl font-bold">{route} ({menetrend.length} járat)</h1>
+
+                <Menetrend adatok={menetrend}/>
             </div>
         </div>
     )
