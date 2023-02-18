94fa0799cae846b6984c97f6913123f76b27e01d
Richard <richarszab56@gmail.com>
2023-02-18 15:47:46 +0100

    Szöveg módosítás
    Így szebb

--- src/components/FooldalSzoveg.jsx
+++ src/components/FooldalSzoveg.jsx
@@ -2,7 +2,7 @@
 const FooldalSzoveg = () => {
   return (
     <div>
-        <h1 className="text-5xl font-bold">Keressen most!</h1>
+        <h1 className="text-5xl font-bold">Keressen járatot!</h1>
         <p className="py-6">Menetrend listázó alkalmazás. Válassza ki honnan hova szeretne eljutni és listázzuk önnek a mai járatokat!</p>
     </div>
   )
