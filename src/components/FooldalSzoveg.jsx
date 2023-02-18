c11cca8e6d3d5fb45ce8a1dc0463e6c6d9337e11
Richard <richarszab56@gmail.com>
2023-02-18 13:39:54 +0100

    Főoldal dinamikusság
    Főoldal szöveg része külön komponens így rendereléskor egyszerűbb lesz váltogatni

---
+++ src/components/FooldalSzoveg.jsx
@@ -0,0 +1,11 @@
+
+const FooldalSzoveg = () => {
+  return (
+    <div>
+        <h1 className="text-5xl font-bold">Keressen most!</h1>
+        <p className="py-6">Menetrend listázó alkalmazás. Válassza ki honnan hova szeretne eljutni és listázzuk önnek a mai járatokat!</p>
+    </div>
+  )
+}
+
+export default FooldalSzoveg
